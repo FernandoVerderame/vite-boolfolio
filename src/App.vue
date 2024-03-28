e990436efb3be6feedea24e8a8a7d09ad1cfd60f
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-28 16:43:10 +0100

    added axios & api endpoint call projects

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import axios from 'axios';
+const endpoint = 'http://localhost:8000/api/projects/';
 
 export default {
   name: 'Boolfolio',
 
-  components: { AppHeader }
+  data: () => ({ projects: [] }),
+
+  components: { AppHeader },
+
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => {
+        console.log(res.data);
+
+        this.projects = res.data;
+      })
+    }
+  },
+
+  created() {
+    this.fetchProjects();
+  }
 }
 </script>
 
@@ -14,6 +32,13 @@ export default {
 
   <main class="container pt-4">
     <h1>Boolfolio</h1>
+
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        <h4>{{ project.title }}</h4>
+      </li>
+    </ul>
+
   </main>
 
 </template>
