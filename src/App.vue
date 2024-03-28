f056137e17121c4ae3eb084f7e7bca55073c776c
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-28 16:32:11 +0100

    added app header & navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
-  name: 'Boolfolio'
+  name: 'Boolfolio',
+
+  components: { AppHeader }
 }
 </script>
 
 <template>
+
+  <AppHeader />
 
   <main class="container pt-4">
     <h1>Boolfolio</h1>
