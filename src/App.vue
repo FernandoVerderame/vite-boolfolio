04828c6481068c1f87db8cea02cc5e7bdcf2f5c9
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-03-28 18:48:46 +0100

    added app alert

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
 import ProjectsList from './components/projects/ProjectsList.vue';
+import AppAlert from './components/AppAlert.vue';
 import axios from 'axios';
 const endpoint = 'http://localhost:8000/api/projects/';
 
 export default {
   name: 'Boolfolio',
 
-  data: () => ({ projects: [], isLoading: false }),
+  data: () => ({ projects: [], isLoading: false, isAlertOpen: false }),
 
-  components: { AppHeader, ProjectsList },
+  components: { AppHeader, ProjectsList, AppAlert },
 
   methods: {
     fetchProjects() {
@@ -17,8 +18,10 @@ export default {
       axios.get(endpoint).then(res => {
         console.log(res.data);
         this.projects = res.data;
+        this.isAlertOpen = false;
       }).catch(err => {
         console.error(err);
+        this.isAlertOpen = true;
       }).then(() => {
         this.isLoading = false;
       })
@@ -37,6 +40,8 @@ export default {
 
   <main class="container pt-4">
     <h1>Boolfolio</h1>
+
+    <AppAlert :show="isAlertOpen" @close="isAlertOpen = false" @retry="fetchProjects" />
 
     <AppLoader v-if="isLoading" />
     <ProjectsList v-else :projects="projects" />
---
+++ src/components/AppAlert.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: 'AppAlert',
+
+    props: {
+        show: Boolean
+    },
+
+    emits: ['close', 'retry']
+}
+</script>
+
+<template>
+    <div v-if="show" class="alert alert-danger alert-dismissible fade show" role="alert">
+        An error has occurred. <strong class="text-decoration-underline" role="button"
+            @click="$emit('retry')">Retry</strong>
+        <button type="button" class="btn-close" @click="$emit('close')"></button>
+    </div>
+</template>
+
+<style></style>
