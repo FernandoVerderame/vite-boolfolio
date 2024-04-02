e4e57f27cfc9cdcb0aa318eb6b00feae62b1781a
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-04-02 17:01:58 +0200

    added router back button

--- src/pages/NotFoundPage.vue
+++ src/pages/NotFoundPage.vue
@@ -7,6 +7,7 @@ export default {
 <template>
     <div class="not-found">
         <h3>404 | Page Not Found</h3>
+        <button class="btn btn-secondary" @click="$router.back()">Come Back</button>
     </div>
 </template>
 
@@ -14,6 +15,7 @@ export default {
 .not-found {
     height: calc(100vh - 60px);
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
 
