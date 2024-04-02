c48cf46072854667f34cc91d768038d702bce44c
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-04-02 19:26:19 +0200

    added type & technologies

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -38,7 +38,16 @@ export default {
                 style="height: 100px;">
             <div class="card-body">
                 <h5 class="card-title">{{ project.title }}</h5>
+                <div v-if="project.type" class="badge mb-2" :style="{ backgroundColor: project.type.color }">{{
+                project.type.label }}
+                </div>
                 <p class="card-text">{{ isDetail ? project.description : abstract }}</p>
+                <ul v-if="project.technologies?.length">
+                    <li v-for="technology in project.technologies" :key="technology.id"
+                        :style="{ color: technology.color }">
+                        <i :class="`${technology.icon}`">{{ technology.label }}</i>
+                    </li>
+                </ul>
                 <div>Created on {{ pubblicationDate }}</div>
                 <RouterLink v-if="!isDetail" :to="{ name: 'project-detail', params: { slug: project.slug } }"
                     class="btn btn-sm btn-primary">
@@ -48,4 +57,14 @@ export default {
     </div>
 </template>
 
-<style></style>
+<style>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+
+    display: flex;
+    justify-content: center;
+    gap: 10px;
+}
+</style>
