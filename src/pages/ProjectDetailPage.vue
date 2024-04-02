1222acd4717f6cab31727d858ebae4b0f372724a
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-04-02 17:41:12 +0200

    added project detail card

--- src/pages/ProjectDetailPage.vue
+++ src/pages/ProjectDetailPage.vue
@@ -1,11 +1,41 @@
 <script>
+import axios from 'axios';
+import AppLoader from '../components/AppLoader.vue';
+import ProjectCard from '../components/projects/ProjectCard.vue';
+const defaultEndpoint = 'http://localhost:8000/api/projects/';
+
 export default {
-    name: 'ProjectDetailPage'
+    name: 'ProjectDetailPage',
+
+    components: { ProjectCard, AppLoader },
+
+    data: () => ({
+        project: null,
+        isLoading: false
+    }),
+
+    methods: {
+        getProject() {
+            this.isLoading = true;
+            axios.get(defaultEndpoint + this.$route.params.id)
+                .then(res => { this.project = res.data; })
+                .catch(err => { console.error(err.message) })
+                .then(() => { this.isLoading = false; })
+        }
+    },
+
+    created() {
+        this.getProject();
+    }
 }
 </script>
 
 <template>
-    <h1>Project Detail</h1>
+
+    <AppLoader v-if="isLoading && !project" />
+
+    <ProjectCard v-if="!isLoading && project" :project="project" />
+
 </template>
 
 <style></style>
