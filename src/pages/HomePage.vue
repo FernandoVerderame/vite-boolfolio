1101494b3026cef278f0c97f1d7ce45a5eedaa1a
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-04-02 15:56:29 +0200

    added homepage

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,61 @@
+<script>
+import ProjectsList from '../components/projects/ProjectsList.vue';
+import AppAlert from '../components/AppAlert.vue';
+import BasePagination from '../components/BasePagination.vue';
+import axios from 'axios';
+const defaultEndpoint = 'http://localhost:8000/api/projects/';
+
+export default {
+    name: 'HomePage',
+
+    data: () => ({
+        projects: {
+            data: [],
+            links: []
+        },
+        isLoading: false,
+        isAlertOpen: false
+    }),
+
+    components: { ProjectsList, AppAlert, BasePagination },
+
+    methods: {
+        fetchProjects(endpoint) {
+            this.isLoading = true;
+            axios.get(endpoint ?? defaultEndpoint)
+                .then(res => {
+                    const { data, links } = res.data;
+
+                    this.projects = { data, links };
+                    this.isAlertOpen = false;
+                }).catch(err => {
+                    console.error(err);
+                    this.isAlertOpen = true;
+                }).then(() => {
+                    this.isLoading = false;
+                })
+        }
+    },
+
+    created() {
+        this.fetchProjects();
+    }
+}
+</script>
+
+<template>
+
+    <h1>Boolfolio</h1>
+
+    <AppAlert :show="isAlertOpen" @close="isAlertOpen = false" @retry="fetchProjects" />
+
+    <AppLoader v-if="isLoading" />
+
+    <div v-else>
+        <ProjectsList :projects="projects.data" />
+        <BasePagination :links="projects.links" @change-page="fetchProjects" />
+    </div>
+
+</template>
+
+<style></style>
