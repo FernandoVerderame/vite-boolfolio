b512c56db3fb196c690de04042f1e2b0f26f94c8
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-04-03 14:29:30 +0200

    added type projects page

---
+++ src/pages/TypeProjectsPage.vue
@@ -0,0 +1,53 @@
+<script>
+import axios from 'axios';
+import { store } from '../data/store';
+const baseUri = 'http://localhost:8000/api';
+import ProjectsList from '../components/projects/ProjectsList.vue';
+
+export default {
+    name: 'TypeProjectsPage',
+
+    components: { ProjectsList },
+
+    data: () => ({
+        projects: [],
+        typeLabel: '',
+        store
+    }),
+
+    methods: {
+        fetchProjects() {
+            store.isLoading = true;
+
+            axios.get(`${baseUri}/types/${this.$route.params.slug}/projects/`)
+                .then(res => {
+                    const { projects, label } = res.data;
+                    this.projects = projects;
+                    this.typeLabel = label;
+                })
+                .catch(err => {
+                    console.error(err.message);
+                    this.$router.push({ name: 'not-found' });
+                })
+                .then(() => {
+                    store.isLoading = false;
+                })
+        }
+    },
+
+    created() {
+        this.fetchProjects();
+    }
+}
+</script>
+
+<template>
+
+    <div v-show="!store.isLoading">
+        <h2>{{ typeLabel }} Projects</h2>
+        <ProjectsList :projects="projects" />
+    </div>
+
+</template>
+
+<style></style>
